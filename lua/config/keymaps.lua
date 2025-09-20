5f44cad73f8552be8cf1aa08f2184f17fbf3e995
ibrahimowins <ibrahimowins@gmail.com>
2025-09-20 11:31:33 +0100

    Switched to LazyNvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 -- Custom keymaps using raw Neovim API (overrides AstroNvim defaults)
 
 local map = vim.keymap.set
@@ -18,10 +21,10 @@ map("n", "<C-a>", "gg<S-v>G", vim.tbl_extend("force", opts, { desc = "Select all
 -- ==============================
 map("n", "<leader>sv", "<cmd>vsplit<cr>", { desc = "Vertical split" })
 map("n", "<leader>sh", "<cmd>split<cr>", { desc = "Horizontal split" })
-map("n", "<leader>wh", "<C-w>h", { desc = "Move to left window" })
-map("n", "<leader>wj", "<C-w>j", { desc = "Move to bottom window" })
-map("n", "<leader>wk", "<C-w>k", { desc = "Move to top window" })
-map("n", "<leader>wl", "<C-w>l", { desc = "Move to right window" })
+map("n", "<leader>w<Left>", "<C-w>h", { desc = "Move to left window" })
+map("n", "<leader>w<Down>", "<C-w>j", { desc = "Move to bottom window" })
+map("n", "<leader>w<Up>", "<C-w>k", { desc = "Move to top window" })
+map("n", "<leader>w<Right>", "<C-w>l", { desc = "Move to right window" })
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
@@ -50,7 +53,4 @@ map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 -- ==============================
 -- Plugins
 -- ==============================
-map("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
-
--- Git (requires vim-fugitive)
-map("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status" })
+-- map("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
